d0ac394d5af2ed4aaad620c539b134666fdfe502
aria414 <ayng13@yahoo.com>
2021-02-24 18:37:31 -0500

    Commit 2: Reviews nested routes complete

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
-  resources :products
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  resources :reviews
+  resources :products do
+    resources :reviews
+  end
 end
