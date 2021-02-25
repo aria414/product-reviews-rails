de55bfc2cf87e134b37a8193bdaddba089e21df3
aria414 <ayng13@yahoo.com>
2021-02-24 19:16:51 -0500

    Commit 8: Routes changed for Reviews update and delete

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
 
   #Tell the router that for Products we only want index and show, and leave out the others:
   resources :products, only: [:index, :show] do
-    resources :reviews, except: [:show]
+    #Can do index and create... forget the rest. http://localhost:3000/products/1/reviews
+    resources :reviews, except: [:show, :update, :destroy]
   end
+
+  resources :reviews, only: [:update, :destroy]
 end
