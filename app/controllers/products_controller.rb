c621a84ac6192daf1f7d456a03b323fcb3a41f7e
aria414 <ayng13@yahoo.com>
2021-02-24 19:01:20 -0500

    Commit 6: Products controller fixed

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,42 +1,19 @@
 class ProductsController < ApplicationController
-  before_action :set_product, only: [:show, :update, :destroy]
+  before_action :set_product, only: [:show]
 
   # GET /products
   def index
     @products = Product.all
 
-    render json: @products
+    render json: @products, status: 200
   end
 
   # GET /products/1
   def show
-    render json: @product
+    render json: @product, status: 200
   end
 
-  # POST /products
-  def create
-    @product = Product.new(product_params)
 
-    if @product.save
-      render json: @product, status: :created, location: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /products/1
-  def update
-    if @product.update(product_params)
-      render json: @product
-    else
-      render json: @product.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /products/1
-  def destroy
-    @product.destroy
-  end
 
   private
     # Use callbacks to share common setup or constraints between actions.
